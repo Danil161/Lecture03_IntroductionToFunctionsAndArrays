d820c69cf3f6385197d40cbcb712dcdd845ad8bd
Danil <danil.bazarov@inbox.ru>
2023-10-05 14:51:48 +0300

    add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,49 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i< length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+int[] arr = { 1, 5, 4, 6, 3, 5, 4, 2, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
